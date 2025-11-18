1ef946080ef6c015fc60e65420ec3af65756711d
Alberto Dequino <alberto.dequino@unibo.it>
2025-11-18 11:10:48 +0100

    Added test_mm_is_eu

--- tests/magia/mesh/mm_is/src/test.c
+++ tests/magia/mesh/mm_is/src/test.c
@@ -11,6 +11,9 @@
 #include "fsync.h"
 #include "idma.h"
 #include "redmule.h"
+#include "eventunit.h"
+
+#define WAIT_MODE POLLING
 
 /**
  * This test aims to verify the functionality of MAGIA as a systolic array for matrix multiplications,
@@ -47,6 +50,19 @@ int main(void){
     fsync_init(&fsync_ctrl);
     idma_init(&idma_ctrl);
     redmule_init(&redmule_ctrl);
+
+    #if STALLING == 0
+    eu_config_t eu_cfg = {.hartid = hartid};
+    eu_controller_t eu_ctrl = {
+        .base = NULL,
+        .cfg = &eu_cfg,
+        .api = &eu_api,
+    };
+    eu_init(&eu_ctrl);
+    eu_fsync_init(&eu_ctrl, 0);
+    eu_redmule_init(&eu_ctrl, 0);
+    eu_idma_init(&eu_ctrl, 0);
+    #endif
 
     uint32_t y_id = GET_Y_ID(hartid);
     uint32_t x_id = GET_X_ID(hartid);
@@ -105,7 +121,9 @@ int main(void){
     uint32_t axi_addr_x = (uint32_t) x_inp + (y_id * N_SIZE * tile_h_max * 2) + (tile_w_max * x_id * 2);
     
     idma_memcpy_2d(&idma_ctrl, 0, axi_addr_x, obi_addr_x, len_x, std_x, reps_x);
-    idma_wait();
+    #if STALLING == 0
+    eu_idma_wait_a2o(&eu_ctrl, WAIT_MODE);
+    #endif
     
     /**
      * 2a. Initalize the IDMA transfer variables for weight data-tile transfers.
@@ -147,7 +165,9 @@ int main(void){
          * 3a. IDMA to load the weight data-tile for current timeslot
          */
         idma_memcpy_2d(&idma_ctrl, 0, (axi_addr_w + (t_size * i * 2)), obi_addr_w, len_w, std_w, reps_w);
-        idma_wait();
+        #if STALLING == 0
+        eu_idma_wait_a2o(&eu_ctrl, WAIT_MODE);
+        #endif
 
         /**
          * 3b. Load the output data-tile
@@ -158,27 +178,37 @@ int main(void){
         if(x_id == 0){
             if(i % 2){
                 idma_memcpy_2d(&idma_ctrl, 0, axi_addr_y + (i * t_size * 2), obi_addr_y_1, len_y, std_y, reps_y);
-                idma_wait();
+                #if STALLING == 0
+                eu_idma_wait_a2o(&eu_ctrl, WAIT_MODE);
+                #endif
             }
             else{
                 idma_memcpy_2d(&idma_ctrl, 0, axi_addr_y + (i * t_size * 2), obi_addr_y_0, len_y, std_y, reps_y);
-                idma_wait();
+                #if STALLING == 0
+                eu_idma_wait_a2o(&eu_ctrl, WAIT_MODE);
+                #endif
             }  
             //printf("Loaded data from L2: %x, %x, %x, %x", *(volatile uint16_t*)(obi_addr_y), *(volatile uint16_t*)(obi_addr_y + 2), *(volatile uint16_t*)(obi_addr_y + 4), *(volatile uint16_t*)(obi_addr_y + 6));
         }
         else{
-            if(fsync_sync_left(&fsync_ctrl))
-                printf("Error when synchronizing with left tile.\n");
+            fsync_sync_left(&fsync_ctrl);
+            #if STALLING == 0
+            eu_fsync_wait(&eu_ctrl, WAIT_MODE);
+            #endif
 
             if(i % 2){
                 uint32_t src_addr = get_l1_base(hartid - 1) + (tile_h_max * tile_w_max * 2) + (tile_w_max * t_size * 2) + (tile_h_max * t_size * 2);
                 idma_memcpy_1d(&idma_ctrl, 0, src_addr, obi_addr_y_1, tile_h * t_size * 2);
-                idma_wait();
+                #if STALLING == 0
+                eu_idma_wait_a2o(&eu_ctrl, WAIT_MODE);
+                #endif
             }                
             else{
                 uint32_t src_addr = get_l1_base(hartid - 1) + (tile_h_max * tile_w_max * 2) + (tile_w_max * t_size * 2);
                 idma_memcpy_1d(&idma_ctrl, 0, src_addr, obi_addr_y_0, tile_h * t_size * 2);
-                idma_wait();
+                #if STALLING == 0
+                eu_idma_wait_a2o(&eu_ctrl, WAIT_MODE);
+                #endif
             }
             //printf("Received this data: %x, %x, %x, %x", *(volatile uint16_t*)(obi_addr_y), *(volatile uint16_t*)(obi_addr_y + 2), *(volatile uint16_t*)(obi_addr_y + 4), *(volatile uint16_t*)(obi_addr_y + 6));
         }
@@ -189,11 +219,15 @@ int main(void){
          */
         if(i % 2){
             redmule_gemm(&redmule_ctrl, obi_addr_x, obi_addr_w, obi_addr_y_1, (uint16_t) tile_h, (uint16_t) tile_w, (uint16_t) t_size);
-            redmule_wait();
+            #if STALLING == 0
+            eu_redmule_wait(&eu_ctrl, WAIT_MODE);
+            #endif
         }
         else{
             redmule_gemm(&redmule_ctrl, obi_addr_x, obi_addr_w, obi_addr_y_0, (uint16_t) tile_h, (uint16_t) tile_w, (uint16_t) t_size);
-            redmule_wait();
+            #if STALLING == 0
+            eu_redmule_wait(&eu_ctrl, WAIT_MODE);
+            #endif
         }
 
         /**
@@ -203,17 +237,23 @@ int main(void){
         if(x_id == (MESH_X_TILES-1)){
             if(i % 2){
                 idma_memcpy_2d(&idma_ctrl, 1, axi_addr_y + (i * t_size * 2), obi_addr_y_1, len_y, std_y, reps_y);
-                idma_wait();
+                #if STALLING == 0
+                eu_idma_wait_o2a(&eu_ctrl, WAIT_MODE);
+                #endif
             }
             else{
                 idma_memcpy_2d(&idma_ctrl, 1, axi_addr_y + (i * t_size * 2), obi_addr_y_0, len_y, std_y, reps_y);
-                idma_wait();
+                #if STALLING == 0
+                eu_idma_wait_o2a(&eu_ctrl, WAIT_MODE);
+                #endif
             }   
         }
         else{
             //printf("Sending this data: %x, %x, %x, %x", *(volatile uint16_t*)(obi_addr_y), *(volatile uint16_t*)(obi_addr_y + 2), *(volatile uint16_t*)(obi_addr_y + 4), *(volatile uint16_t*)(obi_addr_y + 6));
-            if(fsync_sync_right(&fsync_ctrl))
-                printf("Error when synchronizing with right tile\n");
+            fsync_sync_right(&fsync_ctrl);
+            #if STALLING == 0
+            eu_fsync_wait(&eu_ctrl, WAIT_MODE);
+            #endif
         }
     }
 
@@ -222,6 +262,9 @@ int main(void){
      */
     uint32_t errors=0;
     fsync_sync_row(&fsync_ctrl);
+    #if STALLING == 0
+    eu_fsync_wait(&eu_ctrl, WAIT_MODE);
+    #endif
     if(x_id == MESH_X_TILES - 1){
         uint16_t computed, expected, diff = 0;
         for(uint8_t i = (y_id * tile_h_max); i < (y_id * tile_h_max + tile_h); i++){
