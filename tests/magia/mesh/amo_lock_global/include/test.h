b22b7a32286f825cd6bb07ccb6cff375f03a1f1e
Alberto Dequino <alberto.dequino@unibo.it>
2026-02-20 11:16:46 +0100

    amo lock + amo_lock_naive test results

--- tests/magia/mesh/amo_lock_global/include/test.h
+++ tests/magia/mesh/amo_lock_global/include/test.h
@@ -4,12 +4,12 @@
 //
 // Alberto Dequino <alberto.dequino@unibo.it>
 
-#ifndef _TEST_AMO_LOCK_INCLUDE_GUARD_
-#define _TEST_AMO_LOCK_INCLUDE_GUARD_
+#ifndef _TEST_AMO_LOCK_GLOBAL_INCLUDE_GUARD_
+#define _TEST_AMO_LOCK_GLOBAL_INCLUDE_GUARD_
 
 #include "utils/amo_utils.h"
 
 extern uint32_t lock = 0x12345678;
 extern uint32_t value = NUM_HARTS;
 
-#endif //_TEST_AMO_LOCK_INCLUDE_GUARD_
+#endif //_TEST_AMO_LOCK_GLOBAL_INCLUDE_GUARD_
---
+++ tests/magia/mesh/amo_lock_row/include/test.h
@@ -0,0 +1,15 @@
+// Copyright 2025 University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Alberto Dequino <alberto.dequino@unibo.it>
+
+#ifndef _TEST_AMO_LOCK_ROW_INCLUDE_GUARD_
+#define _TEST_AMO_LOCK_ROW_INCLUDE_GUARD_
+
+#include "utils/amo_utils.h"
+
+extern uint32_t lock = 0x12345678;
+extern uint32_t value [MESH_X_TILES] = {0};
+
+#endif //_TEST_AMO_LOCK_ROW_INCLUDE_GUARD_
