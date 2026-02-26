9b6704c09d393fe8bc3304adb75422c9a430ecd2
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-02-26 13:46:53 +0100

    [targets/magia_v2] compare_utils.h: Introduce tollerance in comparison

--- targets/magia_v2/include/utils/compare_utils.h
+++ targets/magia_v2/include/utils/compare_utils.h
@@ -7,9 +7,12 @@
 #include "magia_tile_utils.h"
 #include "printf_wrapper.h"
 
+#define TOLL 0x0011
+
 static inline bool vector_compare_fp16_bitwise(uintptr_t addr_res, uintptr_t addr_exp, int len) {
     volatile uint16_t val_exp;
     volatile uint16_t val_res;
+    volatile uint16_t abs_diff;
     uint32_t offset;
     bool ret;
 
@@ -19,9 +22,10 @@ static inline bool vector_compare_fp16_bitwise(uintptr_t addr_res, uintptr_t add
 
         val_exp = mmio16(addr_exp + offset);
         val_res = mmio16(addr_res + offset);
+        abs_diff = (val_exp > val_res) ? (val_exp - val_res) : (val_res - val_exp);
 
-        if (val_exp != val_res) {
-            printf("Mismatch at index %d - expected raw: 0x%04x - computed raw: 0x%04x\n", i, val_exp, val_res);
+        if (abs_diff > TOLL) {
+            printf("Mismatch at index %d - expected raw: 0x%04x - computed raw: 0x%04x - abs_diff: 0x%04x\n", i, val_exp, val_res, abs_diff);
             ret = false;
         }
     }
