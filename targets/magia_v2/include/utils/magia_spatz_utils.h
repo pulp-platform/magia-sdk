dc5990c848c714cea7cf4cd353dccd1a525ae54b
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-02-27 09:13:18 +0100

    [targets/magia_v2] spatz: Align also magia_spat_utils.h SDK with recent RTL changes to fix Spatz boot race condition

--- targets/magia_v2/include/utils/magia_spatz_utils.h
+++ targets/magia_v2/include/utils/magia_spatz_utils.h
@@ -79,6 +79,9 @@ static inline void spatz_run_task_with_params(uint32_t spatz_task_addr, uint32_t
 static inline void spatz_init(uint32_t spatz_start_addr) {
     spatz_set_func(spatz_start_addr);
     spatz_clk_en();
+    while (mmio32(SPATZ_READY) == 0){
+        printf("Waiting for Spatz to be ready...\n");
+    };
 }
 
 #endif
