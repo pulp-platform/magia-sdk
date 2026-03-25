3f112440a684376f5071971debf350c67ca48049
TheSSDGuy <lorzuo@gmail.com>
2026-03-25 15:33:51 +0100

    Added pulp target

---
+++ tests/pulp/hello_pulp/include/test.h
@@ -0,0 +1,10 @@
+// Copyright 2025 University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Alberto Dequino <alberto.dequino@unibo.it>
+
+#ifndef _TEST_HELLOWORLD_INCLUDE_GUARD_
+#define _TEST_HELLOWORLD_INCLUDE_GUARD_
+
+#endif //_TEST_MAGIAMESH_INCLUDE_GUARD_
---
+++ tests/pulp/hello_pulp/src/test.c
@@ -0,0 +1,26 @@
+// Copyright 2025 University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Alberto Dequino <alberto.dequino@unibo.it>
+
+#include <stdint.h>
+#include "tile.h"
+
+/**
+ * This test gently asks for each tile to say hello world.
+ */
+int main(void){
+    uint32_t hartid = get_hartid();
+
+    int test = NULL;
+
+    float test_float = -12.0456f;
+    //int test_integer = -123;
+    
+    wait_nop(hartid*10000);
+    printf("[PULP-%d] Hello world! %f\n",hartid, test_float);
+    //printf("Hello world! %d\n", test_integer);
+
+    return 0;
+}
