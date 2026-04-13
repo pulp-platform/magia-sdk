7964cf5bcc735f2b40d7f1d318b004751e40c431
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-04-13 16:30:01 +0200

    [tests/spatz_on_magia] onnx_exp: Add support for multi-tile and tensor as input

--- tests/spatz_on_magia/onnx_exp/include/onnx_exp_params.h
+++ tests/spatz_on_magia/onnx_exp/include/onnx_exp_params.h
@@ -4,10 +4,12 @@
 #include <stdint.h>
 
 typedef struct {
-    uintptr_t addr_input;
-    uintptr_t addr_res;
-    uintptr_t addr_exp;
-    uint32_t len;
+    uintptr_t chunk_input;  /* Tile's input chunk                                       */
+    uintptr_t chunk_res;    /* Tile's computed result chunk                             */
+    uintptr_t chunk_exp;    /* Tile's golden model chunk                                */
+    uint32_t start;         /* Tile's chunk global start index                          */
+    uint32_t end;           /* Tile's chunk global end index                            */
+    uint32_t len;           /* Tile's chunk len                                         */
 } onnx_exp_params_t;
 
 #endif  /* ONNX_EXP_PARAMS_H_ */
