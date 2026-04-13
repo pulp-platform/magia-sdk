28f922cf5d82c92d00118aee055fe5db5e46a9c1
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-04-13 16:46:43 +0200

    [tests/spatz_on_magia] onnx_floor: Add support for multi-tile and tensor as input

--- tests/spatz_on_magia/onnx_floor/spatz_task/onnx_floor_task.c
+++ tests/spatz_on_magia/onnx_floor/spatz_task/onnx_floor_task.c
@@ -8,29 +8,29 @@ int onnx_floor_task(void)
 {
     volatile onnx_floor_params_t *params;
     uintptr_t params_addr;
-    _Float16 *src;
-    _Float16 *dst;
+    _Float16 *X;
+    _Float16 *Y;
     size_t avl;
     size_t vl;
 
     params_addr = mmio32(SPATZ_DATA);
     params = (volatile onnx_floor_params_t *) params_addr;
 
-    src = (_Float16 *)params->addr_input;
-    dst = (_Float16 *)params->addr_res;
+    X = (_Float16 *)params->chunk_X;
+    Y = (_Float16 *)params->chunk_Y;
     avl = params->len;
 
     asm volatile ("fsrm %0" ::"r"(RDN));
 
     for (; avl > 0; avl -= vl) {
         asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
-        asm volatile ("vle16.v v8, (%0)" :: "r"(src));
+        asm volatile ("vle16.v v8, (%0)" :: "r"(X));
         asm volatile ("vfcvt.x.f.v v16, v8");
         asm volatile ("vfcvt.f.x.v v8, v16");
-        asm volatile ("vse16.v v8, (%0)" :: "r"(dst) : "memory");
+        asm volatile ("vse16.v v8, (%0)" :: "r"(Y) : "memory");
 
-        src += vl;
-        dst += vl;
+        X += vl;
+        Y += vl;
     }
 
     return 0;
