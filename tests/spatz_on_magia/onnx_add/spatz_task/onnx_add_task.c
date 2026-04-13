20e5c523ece082777921f1b6656af2597f9cb16f
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-04-13 10:32:22 +0200

    [tests/spatz_on_magia] onnx_add: Add support for Multi-Tile and Tensors as input

--- tests/spatz_on_magia/onnx_add/spatz_task/onnx_add_task.c
+++ tests/spatz_on_magia/onnx_add/spatz_task/onnx_add_task.c
@@ -5,33 +5,33 @@ int onnx_add_task(void)
 {
     volatile onnx_add_params_t *params;
     uintptr_t params_addr;
-    _Float16 *a;
-    _Float16 *b;
-    _Float16 *r;
+    _Float16 *A;
+    _Float16 *B;
+    _Float16 *C;
     size_t avl;
     size_t vl;
 
     params_addr = mmio32(SPATZ_DATA);
     params = (volatile onnx_add_params_t *) params_addr;
 
-    r = (_Float16 *)params->addr_res;
-    a = (_Float16 *)params->addr_a;
-    b = (_Float16 *)params->addr_b;
+    A = (_Float16 *)params->chunk_A;
+    B = (_Float16 *)params->chunk_B;
+    C = (_Float16 *)params->chunk_C;
     avl = params->len;
 
     for (; avl > 0; avl -= vl) {
         asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
 
-        asm volatile ("vle16.v v0, (%0)" :: "r"(a));
-        asm volatile ("vle16.v v8, (%0)" :: "r"(b));
+        asm volatile ("vle16.v v0, (%0)" :: "r"(A));
+        asm volatile ("vle16.v v8, (%0)" :: "r"(B));
 
         asm volatile ("vfadd.vv v16, v0, v8");
 
-        asm volatile ("vse16.v v16, (%0)" :: "r"(r));
+        asm volatile ("vse16.v v16, (%0)" :: "r"(C));
 
-        a += vl;
-        b += vl;
-        r += vl;
+        A += vl;
+        B += vl;
+        C += vl;
     }
 
     return 0;
