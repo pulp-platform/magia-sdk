5f8f708a08c40568b2740fc9560d0e9f4d95b10b
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-04-14 14:15:07 +0200

    [tests/spatz_on_magia] onnx_hardsigmoid: Add support for multi-tile and tensor as input

--- tests/spatz_on_magia/onnx_hardsigmoid/main.c
+++ tests/spatz_on_magia/onnx_hardsigmoid/main.c
@@ -7,29 +7,48 @@
 #include "onnx_hardsigmoid_params.h"
 #include "onnx_hardsigmoid_task_bin.h"
 
+#define HID get_hartid()
+
 static int init_data(void *params)
 {
-    uint32_t offset;
-    volatile onnx_hardsigmoid_params_t *sigmoid_params;
+    volatile onnx_hardsigmoid_params_t *hardsigmoid_params;
+    uint32_t start;
+    uint32_t chunk;
+    uint32_t left;
+    uint32_t len;
+    uint32_t end;
 
-    sigmoid_params = (volatile onnx_hardsigmoid_params_t *) params;
-    for (int i = 0; i < LEN; i++) {
-        offset = i * sizeof(float16);
+    hardsigmoid_params = (volatile onnx_hardsigmoid_params_t *) params;
 
-        mmio_fp16(EXP_BASE + offset) = expected_vec[i];
-        mmio_fp16(INPUT_BASE + offset) = input_vec[i];
-        mmio_fp16(RES_BASE + offset) = 0;
+    chunk = TENSOR_LEN / NUM_HARTS;
+    left = TENSOR_LEN % NUM_HARTS;
+    start = HID * chunk + (HID < left ? HID : left);
+    end = start + chunk + (HID < left ? 1 : 0);
+    len = end - start;
+
+    for (int i = 0; i < len; i++) {
+        int global_idx;
+        uint32_t offset;
+
+        global_idx = start + i;
+        offset =  i * sizeof(float16);
+
+        mmio_fp16(CHUNK_X_BASE + offset) = X[global_idx];
+        mmio_fp16(CHUNK_G_BASE + offset) = G[global_idx];
+        mmio_fp16(CHUNK_Y_BASE + offset) = 0;
     }
 
     mmio_fp16(ALPHA_BASE) = alpha;
     mmio_fp16(BETA_BASE) = beta;
 
-    sigmoid_params->addr_input = INPUT_BASE;
-    sigmoid_params->addr_alpha = ALPHA_BASE;
-    sigmoid_params->addr_beta = BETA_BASE;
-    sigmoid_params->addr_res = RES_BASE;
-    sigmoid_params->addr_exp = EXP_BASE;
-    sigmoid_params->len = LEN;
+    hardsigmoid_params->chunk_X = CHUNK_X_BASE;
+    hardsigmoid_params->chunk_Y = CHUNK_Y_BASE;
+    hardsigmoid_params->chunk_G = CHUNK_G_BASE;
+    hardsigmoid_params->alpha = ALPHA_BASE;
+    hardsigmoid_params->beta = BETA_BASE;
+    hardsigmoid_params->start = start;
+    hardsigmoid_params->len = len;
+    hardsigmoid_params->end = end;
 
     return 0;
 }
@@ -62,9 +81,9 @@ static int run_spatz_task()
 
 static bool check_result(void *params)
 {
-    volatile onnx_hardsigmoid_params_t *sigmoid_params;
-    sigmoid_params = (volatile onnx_hardsigmoid_params_t *) params;
-    return vector_compare_fp16_bitwise(sigmoid_params->addr_res, sigmoid_params->addr_exp, sigmoid_params->len);
+    volatile onnx_hardsigmoid_params_t *hardsigmoid_params;
+    hardsigmoid_params = (volatile onnx_hardsigmoid_params_t *) params;
+    return chunk_compare_fp16_bitwise(hardsigmoid_params->chunk_Y, hardsigmoid_params->chunk_G, hardsigmoid_params->start, hardsigmoid_params->len);
 }
 
 static bool run_test()
@@ -77,21 +96,21 @@ static bool run_test()
 
     ret = init_data(params);
     if (ret != 0) {
-        printf("[CV32] Params initialization failed with error: %d\n", ret);
+        printf("[CV32 (%d)] Params initialization failed with error: %d\n", HID, ret);
         return ret;
     }
 
     ret = run_spatz_task();
     if (ret != 0) {
-        printf("[CV32] Spatz task FAILED with error: %d", ret);
+        printf("[CV32 (%d)] Spatz task FAILED with error: %d", HID, ret);
         return ret;
     }
 
     check = check_result(params);
     if (check) {
-        printf("[CV32] Test SUCCESS\n");
+        printf("[CV32 (%d)] Test SUCCESS\n", HID);
     } else {
-        printf("[CV32] Test FAILED\n");
+        printf("[CV32 (%d)] Test FAILED\n", HID);
         ret = -1;
     }
 
@@ -102,11 +121,11 @@ int main(void)
 {
     int ret;
 
-    printf("\n##################################### ONNX_HARDSIGMOID TEST #####################################\n\n");
+    if (HID == 0) printf("\n############################### ONNX_HARDSIGMOID TEST on %d Tiles ################################\n\n", NUM_HARTS);
 
     ret = run_test();
 
-    printf("\n##########################################################################################\n\n");
+    if (HID == 0) printf("\n##########################################################################################\n\n");
 
     return ret;
 }
