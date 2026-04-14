612ef111826b3c9c2f1e16f54967df8f0e5d3434
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-04-14 16:31:40 +0200

    [tests/spatz_on_magia] onnx_hardswish: Add support for multi-tile and tensor as input

--- tests/spatz_on_magia/onnx_hardswish/test_data/generator.py
+++ tests/spatz_on_magia/onnx_hardswish/test_data/generator.py
@@ -5,39 +5,57 @@ import numpy as np
 import onnxruntime as ort
 
 
+def positive_int(value):
+    try:
+        ival = int(value)
+
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"'{value}' is not a valid Integer number.")
+
+    if ival <= 0:
+        raise argparse.ArgumentTypeError(f"Value must be positive ({value}).")
+
+    return ival
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Generator of Input Data and Golden Model for ONNX Hardswish test")
-    parser.add_argument("length", type=int, help="Input vector length")
-    args = parser.parse_args()
 
+    parser.add_argument("N", type=positive_int, help="Batch size")
+    parser.add_argument("C", type=positive_int, help="Number of input channels")
+    parser.add_argument("H", type=positive_int, help="Spatial height dimension")
+    parser.add_argument("W", type=positive_int, help="Spatial width dimension")
+
+    args = parser.parse_args()
     return args
 
 
-def generate_input_data(length):
-    input = (np.random.randn(length)).astype(np.float16)
-    return input
+def generate_input_data(args):
+    shape = (args.N, args.C, args.H, args.W)
+    X = np.random.randn(*shape).astype(np.float16)
+    return X
 
 
-def run_onnx_hardswish(input):
-    input_info = onnx.helper.make_tensor_value_info('I', onnx.TensorProto.FLOAT16, input.shape)
-    output_info = onnx.helper.make_tensor_value_info('O', onnx.TensorProto.FLOAT16, input.shape)
+def run_onnx_hardswish(X):
+    X_info = onnx.helper.make_tensor_value_info('X', onnx.TensorProto.FLOAT16, X.shape)
+    Y_info = onnx.helper.make_tensor_value_info('Y', onnx.TensorProto.FLOAT16, X.shape)
 
     opset = onnx.helper.make_operatorsetid("", 22)
-    node_def = onnx.helper.make_node('HardSwish', ['I'], ['O'])
-    graph_def = onnx.helper.make_graph([node_def], 'onnx-hardswish-test', [input_info], [output_info])
+    node_def = onnx.helper.make_node('HardSwish', ['X'], ['Y'])
+    graph_def = onnx.helper.make_graph([node_def], 'onnx-hardswish-test', [X_info], [Y_info])
     model_def = onnx.helper.make_model(graph_def, producer_name='onnx-generator', opset_imports=[opset])
 
     ses = ort.InferenceSession(model_def.SerializePartialToString())
-    res = ses.run(None, {'I':input})
+    res = ses.run(None, {'X': X})
 
     return res[0]
 
 
 def format_array(array):
-    return "{ " + ", ".join(f"{x:f}f" for x in array) + " }"
+    return "{ " + ", ".join(f"{x:f}f" for x in array.flatten()) + " }"
 
 
-def generate_header_file(len, input, expected, filename="data.h"):
+def generate_header_file(args, X, G, filename="data.h"):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(script_dir, filename)
 
@@ -46,10 +64,13 @@ def generate_header_file(len, input, expected, filename="data.h"):
         f.write(f"#ifndef DATA_H_\n")
         f.write(f"#define DATA_H_\n\n")
 
-        f.write(f"#define LEN {len}\n\n")
+        f.write(f"#define BATCH {args.N}\n")
+        f.write(f"#define CHANNELS {args.C}\n")
+        f.write(f"#define HEIGHT {args.H}\n")
+        f.write(f"#define WIDTH {args.W}\n\n")
 
-        f.write(f"static const float16 input_vec[] = {format_array(input)};\n")
-        f.write(f"static const float16 expected_vec[] = {format_array(expected)};\n\n")
+        f.write(f"static const float16 X[] = {format_array(X)};\n")
+        f.write(f"static const float16 G[] = {format_array(G)};\n\n")
 
         f.write(f"#endif   /* DATA_H_ */\n")
 
@@ -57,13 +78,13 @@ def generate_header_file(len, input, expected, filename="data.h"):
 def main():
     args = parse_args()
 
-    input = generate_input_data(args.length)
+    X = generate_input_data(args)
 
-    expected = run_onnx_hardswish(input)
+    G = run_onnx_hardswish(X)
 
-    generate_header_file(args.length, input, expected)
+    generate_header_file(args, X, G)
 
-    print(f"File 'data.h' successfully generated")
+    print(f"File 'data.h' successfully generated with [N:{args.N}, C:{args.C}, H:{args.H}, W:{args.W}]")
 
 
 if __name__ == "__main__":
