2e71f503e938b7aea596cb607cdffd76ceb6bede
Alberto Dequino <alberto.dequino@unibo.it>
2026-05-05 09:44:20 +0200

    First data generator for FFTs

---
+++ tests/spatz_on_magia/fft_fs/test_data/data.h
@@ -0,0 +1,11 @@
+/* Automatically generated header file for Spatz ONNX testing */
+#ifndef DATA_H_
+#define DATA_H_
+
+#define N 8
+#define TW 12.0
+static const float16 A[] = { -0.634766f, -0.962402f, 1.067383f, 0.551270f, 0.083801f, -1.112305f, -1.305664f, 0.901367f };
+static const float16 W[] = { 1.000000f, 1.000000f, 1.000000f, 1.000000f, 1.000000f, 0.000000f, 1.000000f, 0.000000f, 1.000000f, 0.707031f, 0.000000f, -0.707031f };
+static const float16 G[] = { 0.843750f, 0.227783f, 1.159180f, -0.020370f, -0.487305f, 0.751953f, 1.037109f, -0.750488f };
+
+#endif  /* DATA_H_ */
