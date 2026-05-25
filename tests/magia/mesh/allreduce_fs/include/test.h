25b8c4fe754491cf640cf6af3903a8a193a9cb8d
Victor Isachi <victor.isachi@unibo.it>
2026-05-25 17:47:44 +0200

    Added AllReduce Workload that uses FractalSync

---
+++ tests/magia/mesh/allreduce_fs/include/test.h
@@ -0,0 +1,10 @@
+// Copyright 2025 University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Alberto Dequino <alberto.dequino@unibo.it>
+
+#ifndef _TEST_ALLREDUCE_FS_INCLUDE_GUARD_
+#define _TEST_ALLREDUCE_FS_INCLUDE_GUARD_
+
+#endif //_TEST_ALLREDUCE_FS_INCLUDE_GUARD_
