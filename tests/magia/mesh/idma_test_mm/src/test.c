f5402f49933ba7ec57ed24cf65ee36b124482aee
Alberto Dequino <alberto.dequino@unibo.it>
2026-06-03 09:40:06 +0200

    Fixed modified files format

--- tests/magia/mesh/idma_test_mm/src/test.c
+++ tests/magia/mesh/idma_test_mm/src/test.c
@@ -16,7 +16,7 @@
  *
  * Authors: Luca Balboni <luca.balboni10@studio.unibo.it>
  *         Based on idma_test.c by Victor Isachi
- * 
+ *
  * MAGIA iDMA Test using Memory-Mapped Control
  */
 
@@ -28,36 +28,36 @@
 #include "eventunit.h"
 #include "fsync.h"
 
+#define X_BASE      get_l1_base(get_hartid())
+#define Y_BASE      get_l1_base(get_hartid() + 12288)
 
-#define X_BASE get_l1_base(get_hartid())
-#define Y_BASE get_l1_base(get_hartid() + 12288)
+#define M_SIZE      (96)
+#define N_SIZE      (64)
 
-#define M_SIZE (96)
-#define N_SIZE (64)
-
-#define VERBOSE (0)
+#define VERBOSE     (0)
 
 #define WAIT_CYCLES (10)
 
 #define CONCURRENT
 
-int main(void) {
-  /** 
-   * 0. Get the mesh-tile's hartid, mesh-tile coordinates and define its L1 base, 
-   * also initialize the controllers for the idma and fsync.
-   */
-  uint32_t hartid = get_hartid();
+int main(void)
+{
+    /**
+     * 0. Get the mesh-tile's hartid, mesh-tile coordinates and define its L1 base,
+     * also initialize the controllers for the idma and fsync.
+     */
+    uint32_t hartid = get_hartid();
 
-  idma_config_t idma_cfg = {.hartid = hartid};
-  idma_controller_t idma_ctrl = {
-      .base = NULL,
-      .cfg = &idma_cfg,
-      .api = &idma_api,
-  };
+    idma_config_t idma_cfg      = {.hartid = hartid};
+    idma_controller_t idma_ctrl = {
+        .base = NULL,
+        .cfg  = &idma_cfg,
+        .api  = &idma_api,
+    };
 
-  idma_init(&idma_ctrl);
+    idma_init(&idma_ctrl);
 
-  /* Init FractalSync */
+    /* Init FractalSync */
     fsync_config_t fsync_cfg      = {.hartid = hartid};
     fsync_controller_t fsync_ctrl = {
         .base = NULL,
@@ -66,80 +66,82 @@ int main(void) {
     };
     fsync_init(&fsync_ctrl);
 
-  uint32_t y_id = GET_Y_ID(hartid);
-  uint32_t x_id = GET_X_ID(hartid);
-  uint32_t l1_tile_base = get_l1_base(hartid);
+    uint32_t y_id         = GET_Y_ID(hartid);
+    uint32_t x_id         = GET_X_ID(hartid);
+    uint32_t l1_tile_base = get_l1_base(hartid);
 
-  #if STALLING == 0
-  eu_config_t eu_cfg = {.hartid = hartid};
-  eu_controller_t eu_ctrl = {
-      .base = NULL,
-      .cfg = &eu_cfg,
-      .api = &eu_api,
-  };
+#if STALLING == 0
+    eu_config_t eu_cfg      = {.hartid = hartid};
+    eu_controller_t eu_ctrl = {
+        .base = NULL,
+        .cfg  = &eu_cfg,
+        .api  = &eu_api,
+    };
 
-  eu_init(&eu_ctrl);
-  eu_clear_events(0xFFFFFFFF);
-  eu_idma_init(&eu_ctrl, 0);
-  eu_fsync_init(&eu_ctrl, 0);
-  #endif
+    eu_init(&eu_ctrl);
+    eu_clear_events(0xFFFFFFFF);
+    eu_idma_init(&eu_ctrl, 0);
+    eu_fsync_init(&eu_ctrl, 0);
+#endif
 
-  fsync_sync_level(&fsync_ctrl, MAX_SYNC_LVL - 1, 0);
-  #if STALLING == 0
-  eu_fsync_wait(&eu_ctrl, WFE);
-  #endif
+    fsync_sync_level(&fsync_ctrl, MAX_SYNC_LVL - 1, 0);
+#if STALLING == 0
+    eu_fsync_wait(&eu_ctrl, WFE);
+#endif
 
+    uint32_t dst_addr;
+    uint32_t src_addr;
+    uint32_t len;
 
-  uint32_t dst_addr;
-  uint32_t src_addr;
-  uint32_t len;
+    dst_addr = (uint32_t)X_BASE;
+    src_addr = (uint32_t)x_inp;
+    len      = (uint32_t)(M_SIZE * N_SIZE * 2); // 2 Bytes per element
 
-  dst_addr = (uint32_t)X_BASE;
-  src_addr = (uint32_t)x_inp;
-  len      = (uint32_t)(M_SIZE*N_SIZE*2); // 2 Bytes per element
+    printf("iDMA moving data from L2 to L1...\n");
+    idma_memcpy_1d(&idma_ctrl, 0, src_addr, dst_addr, len);
+#if STALLING == 0
+    eu_idma_wait_a2o(&eu_ctrl, WFE);
+#endif
 
-  printf("iDMA moving data from L2 to L1...\n");
-  idma_memcpy_1d(&idma_ctrl, 0, src_addr, dst_addr, len);
-  #if STALLING == 0
-  eu_idma_wait_a2o(&eu_ctrl, WFE);
-  #endif
+    dst_addr = (uint32_t)w_out;
+    src_addr = (uint32_t)X_BASE;
+    len      = (uint32_t)(M_SIZE * N_SIZE * 2); // 2 Bytes per element
+    printf("iDMA moving data from L1 to L2...\n");
+    idma_memcpy_1d(&idma_ctrl, 1, dst_addr, src_addr, len);
 
-  dst_addr = (uint32_t)w_out;
-  src_addr = (uint32_t)X_BASE;
-  len      = (uint32_t)(M_SIZE*N_SIZE*2); // 2 Bytes per element
-  printf("iDMA moving data from L1 to L2...\n");
-  idma_memcpy_1d(&idma_ctrl, 1, dst_addr, src_addr, len);
+    dst_addr = (uint32_t)Y_BASE;
+    src_addr = (uint32_t)x_inp;
+    len      = (uint32_t)(M_SIZE * N_SIZE * 2); // 2 Bytes per element
+    // Start both transfers concurrently
+    printf("iDMA moving concurrently data from L1 to L2 and from L2 to L1...\n");
+    idma_memcpy_1d(&idma_ctrl, 0, src_addr, dst_addr, len);
 
-  dst_addr = (uint32_t)Y_BASE;
-  src_addr = (uint32_t)x_inp;
-  len      = (uint32_t)(M_SIZE*N_SIZE*2); // 2 Bytes per element
-  // Start both transfers concurrently
-  printf("iDMA moving concurrently data from L1 to L2 and from L2 to L1...\n");
-  idma_memcpy_1d(&idma_ctrl, 0, src_addr, dst_addr, len);
+// Use WFE to wait for both transfers completion
+#if STALLING == 0
+    eu_idma_wait_a2o(&eu_ctrl, WFE);
+#endif
+#if STALLING == 0
+    eu_idma_wait_o2a(&eu_ctrl, WFE);
+#endif
 
-  // Use WFE to wait for both transfers completion
-  #if STALLING == 0
-  eu_idma_wait_a2o(&eu_ctrl, WFE);
-  #endif
-  #if STALLING == 0
-  eu_idma_wait_o2a(&eu_ctrl, WFE);
-  #endif
-  
+    printf("Verifying results...\n");
 
-  printf("Verifying results...\n");
-  
-  unsigned int num_errors = 0;
+    unsigned int num_errors = 0;
 
-  uint16_t detected_l2, expected;
-  for(int i = 0; i < M_SIZE*N_SIZE; i++){
-    detected_l2 = mmio16((uint32_t)(w_out) + 2*i);
-    expected = mmio16((uint32_t)(x_inp) + 2*i);
-    if((detected_l2 != expected)){
-      num_errors++;
-      printf("**ERROR**: DETECTED L2[%d](=0x%x) != EXPECTED[%d](=0x%x)\n", i, detected_l2, i, expected);
+    uint16_t detected_l2, expected;
+    for (int i = 0; i < M_SIZE * N_SIZE; i++) {
+        detected_l2 = mmio16((uint32_t)(w_out) + 2 * i);
+        expected    = mmio16((uint32_t)(x_inp) + 2 * i);
+        if ((detected_l2 != expected)) {
+            num_errors++;
+            printf("**ERROR**: DETECTED L2[%d](=0x%x) != EXPECTED[%d](=0x%x)\n",
+                   i,
+                   detected_l2,
+                   i,
+                   expected);
+        }
     }
-  }
-  printf("Finished test with %d errors\n", num_errors);
+    printf("Finished test with %d errors\n", num_errors);
 
-  return num_errors;
+    return num_errors;
 }
