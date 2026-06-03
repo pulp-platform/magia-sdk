c7c30578faf2fd017dbd3e0a4eb01b82f3f19547
Victor Isachi <victor.isachi@unibo.it>
2026-06-03 14:18:56 +0200

    Added utils for NoC/AMO-based global, row and column synchronization patterns

--- targets/magia_v1/include/tile.h
+++ targets/magia_v1/include/tile.h
@@ -21,6 +21,6 @@
 // #include "utils/attention_utils.h"
 #include "utils/performance_utils.h"
 #include "utils/amo_utils.h"
-#include "utils/eu_isa_utils.h"
+#include "utils/nsync_utils.h"
 
 #endif //_TILE_INCLUDE_GUARD_
