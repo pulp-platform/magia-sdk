882fe3c692a598713e0328f3f7bfd5f7f84be2d4
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-06-15 16:25:21 +0200

    [kernles/concat] Use allocator for pointers in L1

--- kernels/concat/fp16/spatz/src/concat_fp16_spatz.c
+++ kernels/concat/fp16/spatz/src/concat_fp16_spatz.c
@@ -1,75 +1,123 @@
 #include <stdint.h>
+#include <errno.h>
 
 #include "eventunit.h"
 #include "tile.h"
 
 #include "concat_fp16_spatz.h"
-#include "concat_fp16_spatz_mem_layout.h"
 #include "concat_fp16_spatz_params.h"
 #include "concat_fp16_spatz_task_bin.h"
 
 #define HID get_hartid()
 
-static int init_input_params(void *params, const float16 *input0, const float16 *input1, const uint32_t in0_transfer_len, const uint32_t in1_transfer_len, const uint32_t iterations)
+static int alloc_l1(void **params, uint32_t in0_transfer_len,  uint32_t in1_transfer_len, uint32_t iterations)
 {
     volatile concat_fp16_spatz_params_t *concat_params;
-    uint32_t iter_start;
-    uint32_t iter_end;
-    uint32_t iter_len;
-    uint32_t shard;
-    uint32_t left;
+    uintptr_t shard_input0;
+    uintptr_t shard_input1;
+    uintptr_t shard_output;
 
+    size_t iter_start;
+    size_t iter_end;
+    size_t iter_len;
+    size_t elems;
+    size_t left;
 
-    concat_params = (volatile concat_fp16_spatz_params_t *) params;
-
-    shard = iterations / NUM_HARTS;
+    elems = iterations / NUM_HARTS;
     left = iterations % NUM_HARTS;
 
-    iter_start = HID * shard + (HID < left ? HID : left);
-    iter_end = iter_start + shard + (HID < left ? 1 : 0);
+    iter_start = HID * elems + (HID < left ? HID : left);
+    iter_end = iter_start + elems + (HID < left ? 1 : 0);
     iter_len = iter_end - iter_start;
 
-    uint32_t l1_in0_idx = 0;
-    uint32_t l1_in1_idx = 0;
-    uint32_t l1_out_idx = 0;
+    l1_alloc_init();
 
-    for (int iter = iter_start; iter < iter_end; iter++) {
-        uint32_t global_idx_in0 = iter * in0_transfer_len;
-        uint32_t global_idx_in1 = iter * in1_transfer_len;
-        uint32_t offset;
+    concat_params = l1_alloc(sizeof(concat_fp16_spatz_params_t));
+    if (!concat_params)
+        return ENOMEM;
 
-        for (int i = 0; i < in0_transfer_len; i++) {
-            offset = l1_in0_idx * sizeof(float16);
-            mmio_fp16(SHARD_INPUT0_BASE + offset) = input0[global_idx_in0 + i];
-            l1_in0_idx++;
-        }
+    shard_input0 = l1_alloc(iter_len * in0_transfer_len * sizeof(float16));
+    if (!shard_input0)
+        return ENOMEM;
 
-        for (int i = 0; i < in1_transfer_len; i++) {
-            offset = l1_in1_idx * sizeof(float16);
-            mmio_fp16(SHARD_INPUT1_BASE + offset) = input1[global_idx_in1 + i];
-            l1_in1_idx++;
-        }
+    shard_input1 = l1_alloc(iter_len * in1_transfer_len * sizeof(float16));
+    if (!shard_input1)
+        return ENOMEM;
 
-        uint32_t output_len = in0_transfer_len + in1_transfer_len;
-        for (int i = 0; i < output_len; i++) {
-            offset = l1_out_idx * sizeof(float16);
-            mmio_fp16(SHARD_OUTPUT_BASE + offset) = 0;
-            l1_out_idx++;
-        }
-    }
+    shard_output = l1_alloc(iter_len * (in0_transfer_len + in1_transfer_len) * sizeof(float16));
+    if (!shard_output)
+        return ENOMEM;
 
-    concat_params->shard_input0 = SHARD_INPUT0_BASE;
-    concat_params->shard_input1 = SHARD_INPUT1_BASE;
-    concat_params->shard_output = SHARD_OUTPUT_BASE;
+    concat_params->shard_input0 = shard_input0;
+    concat_params->shard_input1 = shard_input1;
+    concat_params->shard_output = shard_output;
     concat_params->len_input0 = in0_transfer_len;
     concat_params->len_input1 = in1_transfer_len;
     concat_params->iter_start = iter_start;
     concat_params->iter_len = iter_len;
 
+    *params = (void *) concat_params;
+
     return 0;
 }
 
-static int offload_spatz_task()
+static int init_input_params(void *params, const float16 *input0, const float16 *input1)
+{
+    volatile concat_fp16_spatz_params_t *concat_params;
+    uintptr_t shard_input0;
+    uintptr_t shard_input1;
+    uintptr_t shard_output;
+    size_t iter_start;
+    size_t iter_len;
+    size_t iter_end;
+    size_t len_in0;
+    size_t len_in1;
+
+    concat_params = (volatile concat_fp16_spatz_params_t *) params;
+
+    shard_input0 = concat_params->shard_input0;
+    shard_input1 = concat_params->shard_input1;
+    shard_output = concat_params->shard_output;
+
+    iter_start = concat_params->iter_start;
+    iter_len = concat_params->iter_len;
+    iter_end = iter_start + iter_len;
+    len_in0 = concat_params->len_input0;
+    len_in1 = concat_params->len_input1;
+
+    uint32_t l1_in0_idx = 0;
+    uint32_t l1_in1_idx = 0;
+    uint32_t l1_out_idx = 0;
+
+    for (uint32_t iter = iter_start; iter < iter_end; iter++) {
+        uint32_t global_idx_in0 = iter * len_in0;
+        uint32_t global_idx_in1 = iter * len_in1;
+        uint32_t offset;
+
+        for (uint32_t i = 0; i < len_in0; i++) {
+            offset = l1_in0_idx * sizeof(float16);
+            mmio_fp16(shard_input0 + offset) = input0[global_idx_in0 + i];
+            l1_in0_idx++;
+        }
+
+        for (uint32_t i = 0; i < len_in1; i++) {
+            offset = l1_in1_idx * sizeof(float16);
+            mmio_fp16(shard_input1 + offset) = input1[global_idx_in1 + i];
+            l1_in1_idx++;
+        }
+
+        uint32_t output_len = len_in0 + len_in1;
+        for (uint32_t i = 0; i < output_len; i++) {
+            offset = l1_out_idx * sizeof(float16);
+            mmio_fp16(shard_output + offset) = 0;
+            l1_out_idx++;
+        }
+    }
+
+    return 0;
+}
+
+static int offload_spatz_task(void *params)
 {
     eu_controller_t eu_ctrl;
     eu_config_t eu_cfg;
@@ -81,7 +129,7 @@ static int offload_spatz_task()
     eu_ctrl.api = &eu_api;
 
     spatz_init(SPATZ_BINARY_START);
-    spatz_run_task_with_params(CONCAT_FP16_SPATZ_TASK, CONCAT_FP16_SPATZ_PARAMS_BASE);
+    spatz_run_task_with_params(CONCAT_FP16_SPATZ_TASK, params);
 
     ret = eu_spatz_wait(&eu_ctrl, WFE);
     if (ret == 0) {
@@ -116,11 +164,11 @@ static int store_result(void *params, float16 *concat_result, const uint32_t ite
 
     uint32_t out_idx = 0;
 
-    for (int iter = 0; iter < len; iter++) {
+    for (uint32_t iter = 0; iter < len; iter++) {
         uint32_t global_iter = start + iter;
         uint32_t global_idx_base = global_iter * out_len;
 
-        for (int i = 0; i < out_len; i++) {
+        for (uint32_t i = 0; i < out_len; i++) {
             uint32_t offset = out_idx * sizeof(float16);
             concat_result[global_idx_base + i] = mmio_fp16(shard_output_base + offset);
             out_idx++;
@@ -135,15 +183,19 @@ void MAGIA_concat_fp16_spatz(const float16 *input0, const float16 *input1, float
     int ret;
     volatile concat_fp16_spatz_params_t *params;
 
-    params = (volatile concat_fp16_spatz_params_t *) CONCAT_FP16_SPATZ_PARAMS_BASE;
+    ret = alloc_l1(&params, in0_transfer_len, in1_transfer_len, iterations);
+    if (ret != 0) {
+        printf("[CV32 (%d)] L1 allocation failed with error: %d\n", HID, ret);
+        return;
+    }
 
-    ret = init_input_params(params, input0, input1, in0_transfer_len, in1_transfer_len, iterations);
+    ret = init_input_params(params, input0, input1);
     if (ret != 0) {
         printf("[CV32 (%d)] Params initialization failed with error: %d\n", HID, ret);
         return;
     }
 
-    ret = offload_spatz_task();
+    ret = offload_spatz_task(params);
     if (ret != 0) {
         printf("[CV32 (%d)] Spatz task offloading failed with error: %d\n", HID, ret);
         return;
