7b017ce80c16af28936dfb0a046b9aae113e5fcd
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-06-18 15:44:13 +0200

    [kernles] LayerNorm: Use allocator for pointers in L1

--- deployment/MagiaDeeployTarget/Templates/LayerNormFP16SpatzTemplate.py
+++ deployment/MagiaDeeployTarget/Templates/LayerNormFP16SpatzTemplate.py
@@ -4,11 +4,12 @@ from Deeploy.DeeployTypes import NetworkContext, NodeTemplate, OperatorRepresent
 class _MagiaLayerNormFP16Spatz(NodeTemplate):
     def alignToContext(self, ctxt: NetworkContext,
                        operatorRepresentation: OperatorRepresentation) -> Tuple[NetworkContext, Dict, List[str]]:
-        ctxt.lookup(operatorRepresentation['data_in'])
+        data_in = ctxt.lookup(operatorRepresentation['data_in'])
         ctxt.lookup(operatorRepresentation['weight'])
         ctxt.lookup(operatorRepresentation['bias'])
-
         ctxt.lookup(operatorRepresentation['data_out'])
+
+        operatorRepresentation['input_shape'] = "{" + ", ".join(map(str, data_in.shape)) + "}"
 
         operatorRepresentation['offset'] = 0
 
@@ -16,5 +17,5 @@ class _MagiaLayerNormFP16Spatz(NodeTemplate):
 
 referenceTemplate = _MagiaLayerNormFP16Spatz("""
 // Magia LayerNorm FP 16 Spatz (Name: ${nodeName}, Op: ${nodeOp})
-MAGIA_layernorm_fp16_spatz(${data_in}, ${weight}, ${bias}, ${epsilon}, ${data_out});
+MAGIA_layernorm_fp16_spatz(${data_in}, ${weight}, ${bias}, ${epsilon}, (uint32_t[])${input_shape}, ${data_out});
 """)
--- kernels/layernorm/fp16/spatz/src/layernorm_fp16_spatz.c
+++ kernels/layernorm/fp16/spatz/src/layernorm_fp16_spatz.c
@@ -1,78 +1,129 @@
 #include <stdint.h>
+#include <errno.h>
 
 #include "eventunit.h"
 #include "tile.h"
 
 #include "layernorm_fp16_spatz.h"
-#include "layernorm_fp16_spatz_mem_layout.h"
 #include "layernorm_fp16_spatz_params.h"
 #include "layernorm_fp16_spatz_task_bin.h"
 
 #define HID get_hartid()
 
+static int alloc_l1(void **params, uint32_t input_shape[4], float16 epsilon)
+{
+    volatile layernorm_fp16_spatz_params_t *layernorm_params;
+    uintptr_t shard_X;
+    uintptr_t shard_Y;
+    uintptr_t gamma;
+    uintptr_t beta;
+    uintptr_t eps;
+
+    size_t total_rows;
+    size_t shard;
+    size_t left;
+    size_t r_start;
+    size_t r_end;
+    size_t r_len;
+    size_t w_len;
+    size_t local_shard_size;
+
+    total_rows = input_shape[0] * input_shape[1] * input_shape[2];
+    w_len = input_shape[3];
+
+    shard = total_rows / NUM_HARTS;
+    left = total_rows % NUM_HARTS;
+
+    r_start = HID * shard + (HID < left ? HID : left);
+    r_end = r_start + shard + (HID < left ? 1 : 0);
+    r_len = r_end - r_start;
+
+    local_shard_size = r_len * w_len;
+
+    l1_alloc_init();
+
+    layernorm_params = l1_alloc(sizeof(layernorm_fp16_spatz_params_t));
+    if (!layernorm_params)
+        return ENOMEM;
+
+    shard_X = l1_alloc(local_shard_size * sizeof(float16));
+    if (!shard_X)
+        return ENOMEM;
+
+    shard_Y = l1_alloc(local_shard_size * sizeof(float16));
+    if (!shard_Y)
+        return ENOMEM;
+
+    gamma = l1_alloc(w_len * sizeof(float16));
+    if (!gamma)
+        return ENOMEM;
+
+    beta = l1_alloc(w_len * sizeof(float16));
+    if (!beta)
+        return ENOMEM;
+
+    eps = l1_alloc(sizeof(float16));
+    if (!eps)
+        return ENOMEM;
+
+    layernorm_params->shard_X = shard_X;
+    layernorm_params->shard_Y = shard_Y;
+    layernorm_params->gamma   = gamma;
+    layernorm_params->beta    = beta;
+    layernorm_params->eps     = eps;
+    layernorm_params->r_start = (uint32_t) r_start;
+    layernorm_params->r_len   = (uint32_t) r_len;
+    layernorm_params->w_len   = (uint32_t) w_len;
+
+    *params = (void *) layernorm_params;
+
+    return 0;
+}
+
 static int init_input_params(void *params, const float16 *X, const float16 *scale, const float16 *B, const float16 epsilon)
 {
     volatile layernorm_fp16_spatz_params_t *layernorm_params;
-    uint32_t shard;
-    uint32_t left;
-    uint32_t r_start;
-    uint32_t r_end;
-    uint32_t r_len;
-    uint32_t w_len;
+    uintptr_t shard_X;
+    uintptr_t shard_Y;
+    uintptr_t gamma;
+    uintptr_t beta;
+    uintptr_t eps;
     uint32_t local_idx;
 
     layernorm_params = (volatile layernorm_fp16_spatz_params_t *) params;
-
-    shard = TOTAL_ROWS / NUM_HARTS;
-    left  = TOTAL_ROWS % NUM_HARTS;
-
-    r_start = HID * shard + (HID < left ? HID : left);
-    r_end   = r_start + shard + (HID < left ? 1 : 0);
-    r_len   = r_end - r_start;
-    w_len   = ROW_LEN;
+    shard_X = layernorm_params->shard_X;
+    shard_Y = layernorm_params->shard_Y;
+    gamma = layernorm_params->gamma;
+    beta = layernorm_params->beta;
+    eps = layernorm_params->eps;
 
     local_idx = 0;
-    for (int r = r_start; r < r_end; r++) {
-        uint32_t r_base;
+    for (uint32_t r = layernorm_params->r_start; r < (layernorm_params->r_start + layernorm_params->r_len); r++) {
+        uint32_t r_base = r * layernorm_params->w_len;
 
-        r_base = r * w_len;
+        for (uint32_t i = 0; i < layernorm_params->w_len; i++) {
+            uint32_t global_idx = r_base + i;
+            uint32_t offset = local_idx * sizeof(float16);
 
-        for (int i = 0; i < w_len; i++) {
-            uint32_t global_idx;
-            uint32_t offset;
-
-            global_idx = r_base + i;
-            offset = local_idx * sizeof(float16);
-
-            mmio_fp16(SHARD_X_BASE + offset) = X[global_idx];
-            mmio_fp16(SHARD_Y_BASE + offset) = 0;
+            mmio_fp16(shard_X + offset) = X[global_idx];
+            mmio_fp16(shard_Y + offset) = 0;
 
             local_idx++;
         }
     }
 
-    for (int i = 0; i < ROW_LEN; i++) {
+    for (uint32_t i = 0; i < layernorm_params->w_len; i++) {
         uint32_t offset = i * sizeof(float16);
-
-        mmio_fp16(GAMMA_BASE + offset) = scale[i];
-        mmio_fp16(BETA_BASE + offset) = B[i];
+        mmio_fp16(gamma + offset) = scale[i];
+        mmio_fp16(beta + offset) = B[i];
     }
 
-    mmio_fp16(EPS_BASE) = epsilon;
-
-    layernorm_params->shard_X = SHARD_X_BASE;
-    layernorm_params->shard_Y = SHARD_Y_BASE;
-    layernorm_params->gamma   = GAMMA_BASE;
-    layernorm_params->beta    = BETA_BASE;
-    layernorm_params->eps     = EPS_BASE;
-    layernorm_params->r_start = r_start;
-    layernorm_params->r_len   = r_len;
-    layernorm_params->w_len   = w_len;
+    mmio_fp16(eps) = epsilon;
 
     return 0;
 }
 
-static int offload_spatz_task()
+static int offload_spatz_task(void *params)
 {
     eu_controller_t eu_ctrl;
     eu_config_t eu_cfg;
@@ -84,7 +135,7 @@ static int offload_spatz_task()
     eu_ctrl.api = &eu_api;
 
     spatz_init(SPATZ_BINARY_START);
-    spatz_run_task_with_params(LAYERNORM_FP16_SPATZ_TASK, LAYERNORM_FP16_SPATZ_PARAMS_BASE);
+    spatz_run_task_with_params(LAYERNORM_FP16_SPATZ_TASK, (uint32_t)params);
 
     ret = eu_spatz_wait(&eu_ctrl, WFE);
     if (ret == 0) {
@@ -102,29 +153,20 @@ exit:
 static int store_result(void *params, float16 *Y)
 {
     volatile layernorm_fp16_spatz_params_t *layernorm_params;
-    uint32_t shard_Y_base;
+    uintptr_t shard_Y_base;
     uint32_t local_idx;
 
     layernorm_params = (volatile layernorm_fp16_spatz_params_t *) params;
     shard_Y_base = layernorm_params->shard_Y;
     local_idx = 0;
 
-    for (int r = 0; r < layernorm_params->r_len; r++) {
-        uint32_t r_idx;
-        uint32_t r_base;
+    for (uint32_t r = 0; r < layernorm_params->r_len; r++) {
+        uint32_t global_r = layernorm_params->r_start + r;
+        uint32_t global_row_offset = global_r * layernorm_params->w_len;
 
-        r_idx = layernorm_params->r_start + r;
-        r_base = r_idx * layernorm_params->w_len;
-
-        for (int i = 0; i < layernorm_params->w_len; i++) {
-            uint32_t global_idx;
-            uint32_t offset;
-
-            global_idx = r_base + i;
-            offset = local_idx * sizeof(float16);
-
-            Y[global_idx] = mmio_fp16(shard_Y_base + offset);
-
+        for (uint32_t i = 0; i < layernorm_params->w_len; i++) {
+            uint32_t offset = local_idx * sizeof(float16);
+            Y[global_row_offset + i] = mmio_fp16(shard_Y_base + offset);
             local_idx++;
         }
     }
@@ -132,12 +174,16 @@ static int store_result(void *params, float16 *Y)
     return 0;
 }
 
-void MAGIA_layernorm_fp16_spatz(const float16 *X, const float16 *scale, const float16 *B, const float16 epsilon, float16 *Y)
+void MAGIA_layernorm_fp16_spatz(const float16 *X, const float16 *scale, const float16 *B, const float16 epsilon, uint32_t input_shape[4], float16 *Y)
 {
     int ret;
     volatile layernorm_fp16_spatz_params_t *params;
 
-    params = (volatile layernorm_fp16_spatz_params_t *) LAYERNORM_FP16_SPATZ_PARAMS_BASE;
+    ret = alloc_l1(&params, input_shape, epsilon);
+    if (ret != 0) {
+        printf("[CV32 (%d)] L1 allocation failed with error: %d\n", HID, ret);
+        return;
+    }
 
     ret = init_input_params(params, X, scale, B, epsilon);
     if (ret != 0) {
@@ -145,7 +191,7 @@ void MAGIA_layernorm_fp16_spatz(const float16 *X, const float16 *scale, const fl
         return;
     }
 
-    ret = offload_spatz_task();
+    ret = offload_spatz_task(params);
     if (ret != 0) {
         printf("[CV32 (%d)] Spatz task offloading failed with error: %d\n", HID, ret);
         return;
