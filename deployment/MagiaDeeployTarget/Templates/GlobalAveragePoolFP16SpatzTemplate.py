05f77734243c1540f6d44bcdff798cf26e55959e
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-06-19 11:25:10 +0200

    [kernles] GlobalAveragePool: Use allocator for pointers in L1

--- deployment/MagiaDeeployTarget/Templates/GlobalAveragePoolFP16SpatzTemplate.py
+++ deployment/MagiaDeeployTarget/Templates/GlobalAveragePoolFP16SpatzTemplate.py
@@ -4,13 +4,15 @@ from Deeploy.DeeployTypes import NetworkContext, NodeTemplate, OperatorRepresent
 class _MagiaGlobalAveragePoolFP16Spatz(NodeTemplate):
     def alignToContext(self, ctxt: NetworkContext,
                        operatorRepresentation: OperatorRepresentation) -> Tuple[NetworkContext, Dict, List[str]]:
-        ctxt.lookup(operatorRepresentation['data_in'])
+        data_in = ctxt.lookup(operatorRepresentation['data_in'])
+        ctxt.lookup(operatorRepresentation['data_out'])
 
+        operatorRepresentation['input_shape'] = "{" + ", ".join(map(str, data_in.shape)) + "}"
         operatorRepresentation['offset'] = 0
 
         return ctxt, operatorRepresentation, []
 
 referenceTemplate = _MagiaGlobalAveragePoolFP16Spatz("""
 // Magia GlobalAveragePool FP 16 Spatz (Name: ${nodeName}, Op: ${nodeOp})
-MAGIA_globalaveragepool_fp16_spatz(${data_in}, ${data_out});
+MAGIA_globalaveragepool_fp16_spatz(${data_in}, ${data_out}, (uint32_t[])${input_shape});
 """)
--- kernels/globalaveragepool/fp16/spatz/include/globalaveragepool_fp16_spatz.h
+++ kernels/globalaveragepool/fp16/spatz/include/globalaveragepool_fp16_spatz.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-void MAGIA_globalaveragepool_fp16_spatz(const float16 *X, float16 *Y);
+void MAGIA_globalaveragepool_fp16_spatz(const float16 *X, float16 *Y, uint32_t input_shape[4]);
 
 #endif  /* GLOBALAVERAGEPOOL_FP16_SPATZ_H_ */
