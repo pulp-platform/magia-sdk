aa42aed652069b0b5bdd669d66d2f38d94a9d581
LasagneArrosto <39263051+Dequino@users.noreply.github.com>
2026-06-23 14:11:02 +0200

    Aligned Magia_v1 with Magia_v2 (#41)
    * Realigned magia_v1 with magia_v2
    
    * Fixed format

--- tests/magia/mesh/hello_world/src/test.c
+++ tests/magia/mesh/hello_world/src/test.c
@@ -10,20 +10,21 @@
 /**
  * This test gently asks for each tile to say hello world.
  */
-int main(void){
+int main(void)
+{
     uint32_t hartid = get_hartid();
 
     int test = NULL;
 
-    volatile float16alt a = -12.0456f;
-    volatile float16alt b = 4.0f;
-    volatile float16alt c = (a + b);
-    //int test_integer = -123;
-    
-    uint32_t *v = (uint32_t *) &c; 
-    printf("Hello world! %#x\n", *v);
+    // volatile float16alt a = -12.0456f;
+    // volatile float16alt b = 4.0f;
+    // volatile float16alt c = (a + b);
+    int test_integer = -123;
+
+    // uint32_t *v = (uint32_t *) &c;
+    // printf("Hello world! %#x\n", *v);
     // printf("Hello world! %f\n", c);
-    //printf("Hello world! %d\n", test_integer);
+    printf("Hello world! %d\n", test_integer);
 
     return 0;
 }
