17509ff873b32924f7e17e2dfb1b44fbe655bbca
Francesco Conti <f.conti@unibo.it>
2026-07-08 15:35:14 +0200

    [mglib] some progress, still not satisficatory

--- mglib/src/mg_redmule.c
+++ mglib/src/mg_redmule.c
@@ -47,6 +47,38 @@ void mg_redmule_gemm(redmule_controller_t *ctrl,
     redmule_gemm(ctrl, x, w, y, m, n, k);
 }
 
+void mg_redmule_gemm_enqueue(redmule_controller_t *ctrl,
+                             eu_controller_t *eu,
+                             eu_wait_mode_t mode,
+                             uint32_t x,
+                             uint32_t w,
+                             uint32_t y,
+                             uint16_t m,
+                             uint16_t n,
+                             uint16_t k,
+                             mg_event_t *event,
+                             mg_event_callback_t callback)
+{
+    int32_t id;
+    while ((id = redmule_acquire(ctrl)) < 0) {
+        // Hardware queue (depth 2) is full: drain one completion pulse to
+        // free a slot before retrying the acquire.
+        if (eu32_redmule_wait(eu, mode)) {
+            mg_redmule_completed++;
+        }
+    }
+    mg_event_init(event, id, callback);
+    redmule16_gemm_enqueue(ctrl, x, w, y, m, n, k);
+}
+
+void mg_redmule_gemm_commit(redmule_controller_t *ctrl) {
+    redmule16_gemm_commit(ctrl);
+}
+
+void mg_redmule_gemm_start(redmule_controller_t *ctrl) {
+    redmule16_gemm_start(ctrl);
+}
+
 void mg_redmule_wait(eu_controller_t *eu, eu_wait_mode_t mode, mg_event_t *event)
 {
     uint8_t target = (uint8_t)(event->id + 1);
