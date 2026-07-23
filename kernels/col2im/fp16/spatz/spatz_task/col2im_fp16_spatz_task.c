68aa8a147293fcbcabca3f6949938b05066140b9
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-07-23 16:46:35 +0200

    [kernels] Col2Im: Improve logic

--- kernels/col2im/fp16/spatz/spatz_task/col2im_fp16_spatz_task.c
+++ kernels/col2im/fp16/spatz/spatz_task/col2im_fp16_spatz_task.c
@@ -1,6 +1,35 @@
 #include "tile.h"
 #include "col2im_fp16_spatz_params.h"
 
+static inline void col2im_row_acc(const _Float16 *src, _Float16 *dst, uint32_t count, uint32_t stride_w)
+{
+    size_t avl = count;
+    size_t vl;
+
+    if (stride_w == 1) {
+        for (; avl > 0; avl -= vl) {
+            asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
+            asm volatile ("vle16.v v0, (%0)" :: "r"(src));
+            asm volatile ("vle16.v v8, (%0)" :: "r"(dst));
+            asm volatile ("vfadd.vv v0, v0, v8");
+            asm volatile ("vse16.v v0, (%0)" :: "r"(dst) : "memory");
+            src += vl;
+            dst += vl;
+        }
+    } else {
+        size_t stride_bytes = stride_w * sizeof(_Float16);
+        for (; avl > 0; avl -= vl) {
+            asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
+            asm volatile ("vle16.v v0, (%0)" :: "r"(src));
+            asm volatile ("vlse16.v v8, (%0), %1" :: "r"(dst), "r"(stride_bytes));
+            asm volatile ("vfadd.vv v0, v0, v8");
+            asm volatile ("vsse16.v v0, (%0), %1" :: "r"(dst), "r"(stride_bytes) : "memory");
+            src += vl;
+            dst += vl * stride_w;
+        }
+    }
+}
+
 int col2im_fp16_spatz_task(void)
 {
     volatile col2im_fp16_spatz_params_t *params;
@@ -50,6 +79,7 @@ int col2im_fp16_spatz_task(void)
     uint32_t l_w = (image_w + 2 * pad_w - dilation_w * (block_w - 1) - 1) / stride_w + 1;
     uint32_t block_volume = block_h * block_w;
     uint32_t image_volume = image_h * image_w;
+    uint32_t l_h = l_len / l_w;
 
     for (uint32_t n = 0; n < batch; n++) {
         for (uint32_t c = 0; c < c_len; c++) {
@@ -62,40 +92,29 @@ int col2im_fp16_spatz_task(void)
                 for (uint32_t kx = 0; kx < block_w; kx++) {
 
                     uint32_t k_idx = ky * block_w + kx;
-                    const _Float16 *p_src = src + local_c_offset_in + (k_idx * l_len);
+                    const _Float16 *src_k = src + local_c_offset_in + (k_idx * l_len);
+                    int base_w = (int) (kx * dilation_w) - (int) pad_w;
 
-                    size_t avl = l_len;
-                    size_t vl;
-                    uint32_t l_offset = 0;
+                    for (uint32_t lh = 0; lh < l_h; lh++) {
+                        int im_h = (int) (lh * stride_h) + (int) (ky * dilation_h) - (int) pad_h;
+                        if (im_h < 0 || im_h >= (int) image_h)
+                            continue;
 
-                    for (; avl > 0; avl -= vl) {
-                        asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
-                        asm volatile ("vle16.v v0, (%0)" :: "r"(p_src));
+                        /* Input columns lw whose im_w = lw*stride_w + base_w falls in [0, image_w). */
+                        int lw_start = (base_w >= 0) ? 0 : (((-base_w) + (int) stride_w - 1) / (int) stride_w);
+                        int hi = (int) image_w - 1 - base_w;
+                        if (hi < 0)
+                            continue;
+                        int lw_end = hi / (int) stride_w + 1;
+                        if (lw_end > (int) l_w)
+                            lw_end = (int) l_w;
+                        if (lw_start >= lw_end)
+                            continue;
 
-                        for (size_t j = 0; j < vl; j++) {
-                            uint32_t curr_l = l_offset + j;
-                            uint32_t curr_lh = curr_l / l_w;
-                            uint32_t curr_lw = curr_l % l_w;
+                        const _Float16 *ps = src_k + lh * l_w + lw_start;
+                        _Float16 *pd = dst_img + (uint32_t) im_h * image_w + (uint32_t) (lw_start * (int) stride_w + base_w);
 
-                            int32_t im_h = (int32_t)(curr_lh * stride_h) + (int32_t)(ky * dilation_h) - (int32_t)pad_h;
-                            int32_t im_w = (int32_t)(curr_lw * stride_w) + (int32_t)(kx * dilation_w) - (int32_t)pad_w;
-
-                            if (im_h >= 0 && im_h < (int32_t)image_h && im_w >= 0 && im_w < (int32_t)image_w) {
-                                uint32_t out_idx = (im_h * image_w) + im_w;
-                                _Float16 val;
-
-                                if (j == 0) {
-                                    asm volatile ("vfmv.f.s %0, v0" : "=f"(val));
-                                } else {
-                                    asm volatile ("vslidedown.vx v8, v0, %0" :: "r"(j));
-                                    asm volatile ("vfmv.f.s %0, v8" : "=f"(val));
-                                }
-
-                                dst_img[out_idx] += val;
-                            }
-                        }
-                        p_src += vl;
-                        l_offset += vl;
+                        col2im_row_acc(ps, pd, (uint32_t) (lw_end - lw_start), stride_w);
                     }
                 }
             }
